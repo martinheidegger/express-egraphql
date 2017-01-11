afd3f29d3b48c19007c59cc411cf86645633c20e
Martin Heidegger <martin.heidegger@gmail.com>
2017-01-11 19:39:32 +0900

    Extracted the reading of the body and parsers into own files for a more readable structure.

---
+++ src/parsers.js
@@ -0,0 +1,46 @@
+/* @flow */
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+import querystring from 'querystring';
+import httpError from 'http-errors';
+
+export type Payload = { [param: string]: mixed };
+export type Parser = (string) => Payload;
+export type Parsers = { [type: string]: Parser };
+
+/**
+ * RegExp to match an Object-opening brace "{" as the first non-space
+ * in a string. Allowed whitespace is defined in RFC 7159:
+ *
+ *     x20  Space
+ *     x09  Horizontal tab
+ *     x0A  Line feed or New line
+ *     x0D  Carriage return
+ */
+const jsonObjRegex = /^[\x20\x09\x0a\x0d]*\{/;
+
+export default {
+  'application/graphql': (body: string): Payload => {
+    return { query: body };
+  },
+  'application/json': (body: string): Payload => {
+    if (jsonObjRegex.test(body)) {
+      /* eslint-disable no-empty */
+      try {
+        return JSON.parse(body);
+      } catch (error) {
+        // Do nothing
+      }
+      /* eslint-enable no-empty */
+    }
+    throw httpError(400, 'POST body sent invalid JSON.');
+  },
+  'application/x-www-form-urlencoded':
+    (body: string): Payload => querystring.parse(body)
+};
---
+++ src/readBody.js
@@ -0,0 +1,60 @@
+/* @flow */
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+import httpError from 'http-errors';
+import zlib from 'zlib';
+import getBody from 'raw-body';
+import type { Request } from './index';
+
+// Return a decompressed stream, given an encoding.
+function decompressed(req, encoding) {
+  switch (encoding) {
+    case 'identity': return req;
+    case 'deflate': return req.pipe(zlib.createInflate());
+    case 'gzip': return req.pipe(zlib.createGunzip());
+  }
+  throw httpError(415, `Unsupported content-encoding "${encoding}".`);
+}
+
+// Read and parse a request body.
+export default function (req: Request, charset: String): Promise<string> {
+  return new Promise((resolve, reject) => {
+    // Assert charset encoding per JSON RFC 7159 sec 8.1
+    if (charset.slice(0, 4) !== 'utf-') {
+      throw httpError(415, `Unsupported charset "${charset.toUpperCase()}".`);
+    }
+
+    // Get content-encoding (e.g. gzip)
+    const contentEncoding = req.headers['content-encoding'];
+    const encoding = typeof contentEncoding === 'string' ?
+      contentEncoding.toLowerCase() :
+      'identity';
+    const length = encoding === 'identity' ?
+      req.headers['content-length'] : null;
+    const limit = 100 * 1024; // 100kb
+    const stream = decompressed(req, encoding);
+
+    // Read body from stream.
+    getBody(stream, { encoding: charset, length, limit }, (err, body) => {
+      if (err) {
+        return reject(
+          err.type === 'encoding.unsupported' ?
+            httpError(415, `Unsupported charset "${charset.toUpperCase()}".`) :
+            httpError(400, `Invalid body: ${err.message}.`)
+        );
+      }
+
+      try {
+        return resolve(body);
+      } catch (error) {
+        return reject(error);
+      }
+    });
+  });
+}
